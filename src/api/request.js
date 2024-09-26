ddec691cbdd9df2b32cca08d9d6994eae63b539a
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-09-26 12:54:34 +0300

    support

--- src/api/request.js
+++ src/api/request.js
@@ -1,13 +1,26 @@
 import axios from "axios";
 const client = axios.create({ baseURL: "https://api.dev1.gomaplus.tech/api" });
 
+// client.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response.status === 401) {
+//       localStorage.removeItem("arhebo-token");
+//       window.location.href = "/";
+//     }
+//   }
+// );
+
 client.interceptors.response.use(
-  (response) => response,
-  (error) => {
+  function (response) {
+    return response;
+  },
+  function (error) {
     if (error.response.status === 401) {
       localStorage.removeItem("arhebo-token");
-      window.location.href = "/";
+      window.location.href = "/auth/login";
     }
+    return Promise.reject(error);
   }
 );
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { SnackbarProvider } from 'notistack';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SnackbarProvider } from "notistack";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const client = new QueryClient()
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const client = new QueryClient();
 root.render(
-    <BrowserRouter>
-      <QueryClientProvider client={client}>
+  <BrowserRouter>
+    <QueryClientProvider client={client}>
       <SnackbarProvider maxSnack={3}>
         <Provider store={store}>
-              <App />
-          </Provider>
-      </SnackbarProvider> 
-      </QueryClientProvider>
-    </BrowserRouter>
+          <App />
+        </Provider>
+      </SnackbarProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
 );
